c790f1362efb35fef1bc9792b5f9f50e1c6d7d4c
Saurabh Agrawal <116831490+saurabhAgrawalGit@users.noreply.github.com>
2026-02-09 04:38:10 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,14 +2,29 @@
 
 cd /home/frappe/frappe-bench
 
-if [ ! -d "sites/frappedocker-production-b75f.up.railway.app" ]; then
-    bench new-site frappedocker-production-b75f.up.railway.app \
+SITE="frappedocker-production-b75f.up.railway.app"
+
+echo "Railway PORT is: $PORT"
+
+# Configure frappe to use Railway port
+bench set-config -g webserver_port $PORT
+bench set-config -g socketio_port $PORT
+
+# Create site if not exists
+if [ ! -d "sites/$SITE" ]; then
+    echo "Creating new site..."
+
+    bench new-site $SITE \
         --admin-password $ADMIN_PASSWORD \
         --db-type postgres \
-        --install-app leave_management \
         --no-mariadb-socket
 
-    bench use frappedocker-production-b75f.up.railway.app
+    bench --site $SITE install-app leave_management
+
+    bench use $SITE
 fi
 
+echo "Starting Frappe on port $PORT"
+
+# Start frappe on correct port
 bench start
