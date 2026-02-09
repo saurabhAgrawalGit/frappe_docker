c279c47cbc48c4b088573b8b8c16d30c89a8caa9
Saurabh Agrawal <116831490+saurabhAgrawalGit@users.noreply.github.com>
2026-02-10 02:42:33 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,22 +4,18 @@ cd /home/frappe/frappe-bench
 
 SITE=$SITE_NAME
 
-echo "Using Railway PORT: $PORT"
+echo "Starting site creation..."
 
-# Set correct port
 bench set-config -g webserver_port $PORT
 
-# Create site if not exists
 if [ ! -d "sites/$SITE" ]; then
-    echo "Creating PostgreSQL site..."
 
     bench new-site $SITE \
         --admin-password $ADMIN_PASSWORD \
         --db-type postgres
 
     bench --site $SITE install-app leave_management
-fi
 
-echo "Starting Frappe..."
+fi
 
 bench serve --port $PORT
